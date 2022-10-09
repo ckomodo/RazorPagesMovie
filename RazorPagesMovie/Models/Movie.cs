efefef3eb00dce8592ab275a4a065dd023be5f5a
chris <chrissakwa@gmail.com>
2022-10-09 12:17:31 -0700

    Add project files.

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie //this is a model for the database
+    {
+        public int ID { get; set; }
+
+        //string Title initialzed to an empty string
+        [StringLength(60, MinimumLength = 3)]//min and max string length
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        //The [Display] attribute specifies the display name of a field.
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)] //get date only, not time, from the date type
+        public DateTime ReleaseDate { get; set; }
+
+        //[Display(Name = "Movie Genre")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]//limits x-ters to be input. Must only use letters
+        // first letter must be uppercase. White spaces are allowed, no numbers & special characters
+        [Required]
+        [StringLength(30)]//maximum length of a string property
+        public string Genre { get; set; } = string.Empty;
+
+        //enables Entity Framework Core to correctly map Price to currency in the database.
+        [Range(1, 100)]//constrains a value to within a specified range
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
+        public String Rating { get; set; } = string.Empty;
+    }
+}
---
+++ RazorPagesMovie/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<RazorPagesMovieContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RazorPagesMovieContext") ?? throw new InvalidOperationException("Connection string 'RazorPagesMovieContext' not found.")));
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+//Redirects HTTP requests to HTTPS.
+app.UseHttpsRedirection();
+
+//Enables static files, such as HTML, CSS, images, and JavaScript to be served.
+app.UseStaticFiles();
+
+//Adds route matching to the middleware pipeline
+app.UseRouting();
+
+//Authorizes a user to access secure resources.
+app.UseAuthorization();
+
+//Configures endpoint routing for Razor Pages.
+app.MapRazorPages();
+
+//Runs the app
+app.Run();
